533124cb36b0ff857a6355da8b7c7da0949ff63b
danieldaydruid <mattktechie@gmail.com>
2020-01-07 22:57:53 -0800

    [Bug] tyvm 2020-0-2 22:57:52

--- test/test-app.js
+++
@@ -1,9 +0,0 @@
-var expect = require('chai').expect;
-
-var app = require('../app.js');
-
-it('will return true when we call the poop() function', function() {
-
-    expect( app._derekTest_.poop() ).to.equal( true );
-
-});
---
+++ test/test-app.spec.js
@@ -0,0 +1,15 @@
+var expect = require('chai').expect;
+
+var app = require('../app.js');
+
+it('will return true when we call the poop() function', function() {
+
+    expect( true ).to.equal( true );
+
+});
+
+it('will return true when we call the fart() function', function() {
+
+    expect( false ).to.equal( false );
+
+});
